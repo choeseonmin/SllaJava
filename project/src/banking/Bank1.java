cc9d51556d7cd0bc849c9108dc730c3d776c4fd4
sllavv <129834569+choeseonmin@users.noreply.github.com>
2023-07-27 01:55:57 +0900

    Update Bank1.java

--- project/src/banking/Bank1.java
+++ project/src/banking/Bank1.java
@@ -2,7 +2,7 @@ package banking;
 
 import java.util.Scanner;
 
-public class Bank1 {
+public class Bank1 { 
 	public static void main(String[] args) {
 
 		BankAccount obj1 = new BankAccount("XYZ", "BA0001");
